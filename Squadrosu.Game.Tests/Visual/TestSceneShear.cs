1ba8b1497b7ab69ebcf9447c8b5b9970f1670845
Speykious <speykious@gmail.com>
2021-12-21 00:59:34 +0100

    Added a shear test scene

--- Squadrosu.Game.Tests/Visual/TestSceneShear.cs
+++ Squadrosu.Game.Tests/Visual/TestSceneShear.cs
@@ -3,19 +3,27 @@
 // Squadrosu! is licensed under the GPL v3. See LICENSE.md for details.
 
 using osu.Framework.Graphics;
+using osuTK;
 
 namespace Squadrosu.Game.Tests.Visual;
 
-public class TestSceneFloatingLogo : SquadrosuTestScene
+public class TestSceneShear : SquadrosuTestScene
 {
     // Add visual tests to ensure correct behaviour of your game: https://github.com/ppy/osu-framework/wiki/Development-and-Testing
     // You can make changes to classes associated with the tests and they will recompile and update immediately.
 
-    public TestSceneFloatingLogo()
+    public TestSceneShear()
     {
-        Add(new FloatingLogo
+        var floatingLogo = new FloatingLogo
         {
             Anchor = Anchor.Centre,
-        });
+            CycleDuration = 2000,
+        };
+
+        Add(floatingLogo);
+
+        AddSliderStep(@"Horizontal Shear", -1f, 1f, 0f,
+            value => floatingLogo.Shear = new Vector2(value, floatingLogo.Shear.Y));
+        AddStep(@"Reset Shear", () => floatingLogo.Shear = new Vector2());
     }
 }
