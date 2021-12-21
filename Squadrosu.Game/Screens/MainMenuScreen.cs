0b85186bf6cb33fe5c8bb74b7e6523574caf9a1f
Speykious <speykious@gmail.com>
2021-12-21 01:15:20 +0100

    Time to start working on the MainMenuScreen

---
+++ Squadrosu.Game/Screens/MainMenuScreen.cs
@@ -0,0 +1,12 @@
+// Copyright (c) SquadroCorp
+// This file is part of Squadrosu!.
+// Squadrosu! is licensed under the GPL v3. See LICENSE.md for details.
+
+using osu.Framework.Screens;
+
+namespace Squadrosu.Game.Screens;
+
+public class MainMenuScreen : Screen
+{
+
+}
