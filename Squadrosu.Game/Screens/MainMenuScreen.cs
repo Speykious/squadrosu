1e78b73ea021bf6ab2a0fc11e5199c0b21a134af
Speykious <speykious@gmail.com>
2021-12-21 13:11:39 +0100

    Started to work on a main menu screen

--- Squadrosu.Game/Screens/MainMenuScreen.cs
+++ Squadrosu.Game/Screens/MainMenuScreen.cs
@@ -2,11 +2,46 @@
 // This file is part of Squadrosu!.
 // Squadrosu! is licensed under the GPL v3. See LICENSE.md for details.
 
+using Microsoft.Build.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
 using osu.Framework.Screens;
+using osuTK;
+using osuTK.Graphics;
+using Squadrosu.Game.Sprites;
 
 namespace Squadrosu.Game.Screens;
 
 public class MainMenuScreen : Screen
 {
+    private Logo logo;
 
+    [BackgroundDependencyLoader]
+    private void load()
+    {
+        InternalChildren = new Drawable[]
+        {
+            new Box
+            {
+                RelativeSizeAxes = Axes.Both,
+                Colour = Color4.DimGray,
+            },
+            logo = new Logo
+            {
+                Anchor = Anchor.CentreLeft,
+                Origin = Anchor.CentreRight,
+                RelativePositionAxes = Axes.Both,
+                Position = new Vector2(-.2f, 0),
+                Shear = new Vector2(.2f, 0),
+            },
+        };
+    }
+
+    protected override void LoadComplete()
+    {
+        base.LoadComplete();
+
+        logo.Delay(200).MoveToX(.5f, 600, Easing.OutCirc);
+    }
 }
