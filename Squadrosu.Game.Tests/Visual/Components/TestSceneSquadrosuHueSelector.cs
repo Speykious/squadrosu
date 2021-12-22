1880df00d5b1614307df2cebdaad5f964f7ee7b0
Speykious <speykious@gmail.com>
2021-12-22 02:20:42 +0100

    Create hue selector test scene

---
+++ Squadrosu.Game.Tests/Visual/Components/TestSceneSquadrosuHueSelector.cs
@@ -0,0 +1,47 @@
+// Copyright (c) SquadroCorp
+// This file is part of Squadrosu!.
+// Squadrosu! is licensed under the GPL v3. See LICENSE.md for details.
+
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osuTK.Graphics;
+using Squadrosu.Game.Components;
+
+namespace Squadrosu.Game.Tests.Visual.Components;
+
+public class TestSceneSquadrosuHueSelector : SquadrosuTestScene
+{
+    // Add visual tests to ensure correct behaviour of your game: https://github.com/ppy/osu-framework/wiki/Development-and-Testing
+    // You can make changes to classes associated with the tests and they will recompile and update immediately.
+
+    [BackgroundDependencyLoader]
+    private void load()
+    {
+        Add(new Container
+        {
+            RelativeSizeAxes = Axes.Both,
+            Anchor = Anchor.Centre,
+            Origin = Anchor.Centre,
+            Children = new Drawable[]
+            {
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Colour = Color4.DarkSlateGray,
+                },
+                new Container
+                {
+                    Width = 500,
+                    Height = 42,
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Child = new SquadrosuHueSelector(),
+                },
+            },
+        });
+    }
+}
