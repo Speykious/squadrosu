1168120107d8b18049787887cbf8673125c66d0c
Speykious <speykious@gmail.com>
2021-12-23 17:17:09 +0100

    Starting to structure out the `Settings`

---
+++ Squadrosu.Game/Settings.cs
@@ -0,0 +1,14 @@
+// Copyright (c) SquadroCorp
+// This file is part of Squadrosu!.
+// Squadrosu! is licensed under the GPL v3. See LICENSE.md for details.
+
+using osu.Framework.Graphics.Textures;
+
+namespace Squadrosu.Game;
+
+public struct Settings
+{
+    public float Hue { get; set; }
+    public Texture MenuBackground { get; set; }
+    public Texture GameBackground { get; set; }
+}
