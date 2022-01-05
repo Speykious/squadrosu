f2ad213ddf9d0784b773e18f292e6dee3cfa20d4
Speykious <speykious@gmail.com>
2022-01-05 11:30:08 +0100

    Add required option overlay to test scenes

--- Squadrosu.Game.Tests/Visual/Screens/TestSceneSplashScreen.cs
+++ Squadrosu.Game.Tests/Visual/Screens/TestSceneSplashScreen.cs
@@ -2,6 +2,7 @@
 // This file is part of Squadrosu!.
 // Squadrosu! is licensed under the GPL v3. See LICENSE.md for details.
 
+using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Screens;
 using Squadrosu.Game.Screens;
@@ -13,11 +14,22 @@ public class TestSceneSplashScreen : SquadrosuTestScene
     // Add visual tests to ensure correct behaviour of your game: https://github.com/ppy/osu-framework/wiki/Development-and-Testing
     // You can make changes to classes associated with the tests and they will recompile and update immediately.
 
-    public TestSceneSplashScreen()
+    private OptionOverlay? optionOverlay;
+
+    private DependencyContainer? dependencies;
+    protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent) =>
+        dependencies = new DependencyContainer(base.CreateChildDependencies(parent));
+
+    [BackgroundDependencyLoader]
+    private void load()
     {
+        optionOverlay = new OptionOverlay();
+        dependencies?.Cache(optionOverlay);
+
         Add(new ScreenStack(new SplashScreen())
         {
             RelativeSizeAxes = Axes.Both
         });
+        Add(optionOverlay);
     }
 }
