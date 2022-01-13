5bd52e979c7d1cd5779dc3da997089cd48f1231a
Speykious <speykious@gmail.com>
2022-01-13 16:51:39 +0100

    Remove some unnecessary `using`s

--- Squadrosu.Game/UI/SquadrosuHueSelector.cs
+++ Squadrosu.Game/UI/SquadrosuHueSelector.cs
@@ -4,11 +4,8 @@
 
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
-using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
-using osu.Framework.Graphics.Colour;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
 using osu.Framework.Graphics.UserInterface;
